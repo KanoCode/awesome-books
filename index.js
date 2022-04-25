c4efa77fbb1048f53a1e5745cb0d7117d2ad4140
KanoCode <kanokanombola@gmail.com>
2022-04-25 16:02:51 +0200

    Add functionality of app

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import {bookList} from './modules/navBar.js';
 
 // console.log()
+import {form} from './modules/addBook.js'
+
+import { updateUi } from './modules/updateUi.js';
+
+
+
+window.addEventListener('load',updateUi)
--- modules/domElements.js
+++ modules/domElements.js
@@ -1,15 +1,25 @@
-const form = document.querySelector('form');
-const bookList = document.getElementById('book-list');
-const AllBookSection = document.getElementById('All-Books');
-const formSection = document.getElementById('formSection');
-const ContactSection = document.getElementById('contact-us');
+const form = document.querySelector("form");
+const bookList = document.getElementById("book-list");
+const AllBookSection = document.getElementById("All-Books");
+const formSection = document.getElementById("formSection");
+const ContactSection = document.getElementById("contact-us");
 
 
-
-const bookArr = [];
 // for the nav section
-const navBtns = document.querySelectorAll('nav button')
+const navBtns = document.querySelectorAll("nav button");
 
+//variables for input fields
 
+const Author = document.getElementById("author");
+const Title = document.getElementById("title");
 
-export {form,bookList,AllBookSection,navBtns,formSection,ContactSection}
+export {
+  form,
+  bookList,
+  AllBookSection,
+  navBtns,
+  formSection,
+  ContactSection,
+  Author,
+  Title
+};
